204aa1d673c83cc437a5a7b8d271efb4ee9dd4cb
Esteban Piedra <esteban@outerspacecoders.com>
2015-07-11 15:47:32 -0600

    Add first_name, last_name, age on User

--- lib/basic_ruby/user.rb
+++ lib/basic_ruby/user.rb
@@ -1,2 +1,22 @@
 class User 
+
+
+	attr_accessor :last_name #Hace la misma funcion que definir setter y getter
+	attr_reader :age #define un metodo de solo lectura (setter)
+	attr_writer :age #define un metodo de solo escritura (getter)
+
+	def first_name=(value)
+		@first_name = value
+	end
+
+
+	def first_name
+		@first_name
+	end
+
+
+
+
+
 end
+
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe User do
+	it "sets the first name" do
+		user = User.new
+		user.first_name = "Esteban"
 
-end
+		expect(user.first_name).to eq "Esteban"
+	end
+
+
+	it "sets the last name" do
+		user = User.new
+		user.last_name = "Piedra"
+
+		expect(user.last_name).to eq "Piedra"
+	end
+
+
+	it "sets the age" do
+		user = User.new
+		user.age = 21
+
+		expect(user.age).to eq 21
+	end
+
+
+
+end 
