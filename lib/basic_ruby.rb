8aa2cf06a521da410404f393c80fd6adb9e38051
Esteban Piedra <esteban@outerspacecoders.com>
2015-07-11 15:06:16 -0600

    Add user class and user_spec file

--- lib/basic_ruby.rb
+++ lib/basic_ruby.rb
@@ -1,4 +1,6 @@
+ #Aqui se debe de color todo el codigo, para ser mas ordenado se hacen require de los archivo de basic_ruby
 require "basic_ruby/version"
+require "basic_ruby/user" 
 
 module BasicRuby
   # Your code goes here...
