78d4c291cc5a6d1480e77e4c65926541fea4e9a1
Esteban Piedra <esteban@outerspacecoders.com>
2015-07-18 15:18:28 -0600

    Add get all, count, find, destroy on user.rb and user_spec.rb

--- lib/basic_ruby/user.rb
+++ lib/basic_ruby/user.rb
@@ -1,9 +1,10 @@
 class User 
 
-
-	attr_accessor :last_name #Hace la misma funcion que definir setter y getter
-	attr_reader :age #define un metodo de solo lectura (setter)
+	attr_accessor :last_name, :birth_data, :address #Hace la misma funcion que definir setter y getter
+	attr_reader :age, :id #define un metodo de solo lectura (setter)
 	attr_writer :age #define un metodo de solo escritura (getter)
+
+	@@users = [] #VARIABLE DE CLASE -  SE DEFINE CON DOBLE @	
 
 	def first_name=(value)
 		@first_name = value
@@ -15,8 +16,33 @@ class User
 	end
 
 
+	# def first_name
+		# "SOBRE ESCRITURA"  el ultimo metodo defino es el que se tomara en cuenta, en caso de sobreescribura de metedos.
+	# end
 
+	def save
+		@id = @@users.count + 1
+		@@users << self # asigna el valor al arreglo - self se sefiere a la instacia del usuario
+	end
 
+	# Busca un usuario en especifico
+	def self.find(id)
+	   @@users.find { |user| user.id == id }
+	end
 
+	#  GET ALL USERS
+	def self.all
+		@@users
+	end
+
+	#  GET COUNT USERS
+	def self.count
+		@@users.count 
+	end
+
+	# DELETE USER
+	def destroy
+		@@users.delete(self)
+	end 
 end
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,29 +1,82 @@
 require 'spec_helper'
 
 describe User do
-	it "sets the first name" do
-		user = User.new
-		user.first_name = "Esteban"
 
-		expect(user.first_name).to eq "Esteban"
-	end
+	before(:each) do
+    	User.class_variable_set(:@@users, [])
+  	end
 
+	describe "instance methods" do 
+	
+		it "sets the first name" do
+			user = User.new
+			user.first_name = "Esteban"
 
-	it "sets the last name" do
-		user = User.new
-		user.last_name = "Piedra"
-
-		expect(user.last_name).to eq "Piedra"
-	end
-
-
-	it "sets the age" do
-		user = User.new
-		user.age = 21
-
-		expect(user.age).to eq 21
-	end
+			expect(user.first_name).to eq "Esteban"
+		end
 
 
+		it "sets the last name" do
+			user = User.new
+			user.last_name = "Piedra"
+
+			expect(user.last_name).to eq "Piedra"
+		end
+
+
+		it "sets the age" do
+			user = User.new
+			user.age = 21
+
+			expect(user.age).to eq 21
+		end
+
+
+		it "Save the user instance into the @@users array" do 
+			user = User.new
+			user.first_name = "Esteban"
+
+			expect(User.count).to eq 0
+			user.save
+			expect(user.id).to eq 1
+			expect(User.count).to eq 1
+		end
+
+		it 'remove the user instance from the @@users array' do 
+			user = User.new
+			user.first_name = "Esteban"
+			user.save
+			expect(User.count).to eq 1
+			user.destroy
+			expect(User.count).to eq 0
+		end
+
+	end 
+
+	describe 'class methods' do
+
+		it 'return an array with all users' do 
+			user = User.new
+			user.first_name = "Esteban"
+			user.save
+
+			expect(User.all).to eq [user]
+		end
+
+		it 'finds the user with te given id' do
+			user = User.new
+			user.first_name = "Esteban"
+			user.save
+
+			user2 = User.new
+			user2.first_name = "Esteban2"
+			user2.save
+
+			expect(User.find(user2.id)).to eq user2
+		end
+
+
+
+	end 
 
 end 
